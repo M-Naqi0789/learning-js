947ca9f3e8f40fb80dab7f0aff7634524c1289b4
Muhammad Naqi <mnaqi0789@gmail.com>
2025-12-31 18:45:00 +0500

    started ts

---
+++ ts notes/src/app.ts
@@ -0,0 +1,4 @@
+// typescript being an addon on js 
+// ts bts - code - lexer - parser - binder - checker - emmiter - generator
+// codes with its range of errors, goes through lexer breaking code into tokens, to parser which converts these tokens into AST or CST (synatax tree), to binder which makes symbol tables or arent pointer or flow nodes, to checker which strictly checks codes twice, finally emmiter generates clean js code
+
